b441fe43ab06d415f3be65d48ce5aa57ffb67e3f
murzakulova <aikenm310@gmail.com>
2022-11-19 16:30:46 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 7;
+        for (int i=0 ; i <10 ; i++) {
+            System.out.println(a+"*" + i+ "=" + a*i);
+
+        }
+    }
+}
